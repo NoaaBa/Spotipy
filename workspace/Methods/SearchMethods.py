d9680c0a99fd3c37818f26d548e6cea990aca99b
Noa <noa27102@gmail.com>
2022-02-09 18:52:42 +0200

    Added 2 new search methods

--- workspace/Methods/SearchMethods.py
+++ workspace/Methods/SearchMethods.py
@@ -3,6 +3,8 @@ from pprint import pprint
 from workspace.ETL.Load import Load
 from workspace.Storage.DataConfigurator import DataConfigurator
 
+file.close()
+
 
 class SearchMethods:
     def get_all_artists(self):
@@ -23,12 +25,37 @@ class SearchMethods:
         file.close()
 
 
-def get_albums_by_artist_name(self, artist):
-    pass
+def get_albums_by_artist_name(self, artist_name):
+    file = open(DataConfigurator.tracks_path + "tracks.json")
+    data_list = json.load(file)
+    albums_list = []
+    for list in data_list:
+        for data in data_list:
+            if artist_name in data:
+                for key in data:
+                    if (key == 'album'):
+                        for album in data[key]:
+                            albums_list.append(album['name'])
+    print(*albums_list, sep=", ")
+    file.close()
 
 
-def get_most_popular_songs_by_artist_name(self, artist):
-    pass
+def get_most_popular_songs_by_artist_name(self, artist_name):
+    file = open(DataConfigurator.tracks_path + "tracks.json")
+    data_list = json.load(file)
+    albums_list = []
+    popular_songs = [0]*10
+    for list in data_list:
+        for data in data_list:
+            if artist_name in data:
+                for i in range(0, 10):
+                    max = (data['song_popularity'])
+                    for j in range(len(popular_songs)):
+                        if popular_songs[j] > max:
+                            max = popular_songs[j]
+                        popular_songs.append(max)
+    print(*popular_songs, sep=", ")
+    file.close()
 
 
 def get_all_songs_in_album(self, album):
