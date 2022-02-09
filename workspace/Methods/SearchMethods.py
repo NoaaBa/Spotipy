f8578b0cad7aa08f6f0b4ead2c28933f01dbb55d
Noa <noa27102@gmail.com>
2022-02-09 18:53:37 +0200

    Removed unused cose

--- workspace/Methods/SearchMethods.py
+++ workspace/Methods/SearchMethods.py
@@ -1,9 +1,5 @@
 import json
-from pprint import pprint
-from workspace.ETL.Load import Load
 from workspace.Storage.DataConfigurator import DataConfigurator
-
-file.close()
 
 
 class SearchMethods:
@@ -11,15 +7,11 @@ class SearchMethods:
         file = open(DataConfigurator.tracks_path + "tracks.json")
         data_list = json.load(file)
         artists_list = []
-        for list in data_list:
-            for data in data_list:
-                # print(f"OOPP {data}")
-                for key in data:
-                    # print(key)
-                    # print(data[key])
-                    if (key == 'artists'):
-                        for artist in data[key]:
-                            artists_list.append(artist['name'])
+        for data in data_list:
+            for key in data:
+                if (key == 'artists'):
+                    for artist in data[key]:
+                        artists_list.append(artist['name'])
         print(*artists_list, sep=", ")
 
         file.close()
@@ -29,13 +21,12 @@ def get_albums_by_artist_name(self, artist_name):
     file = open(DataConfigurator.tracks_path + "tracks.json")
     data_list = json.load(file)
     albums_list = []
-    for list in data_list:
-        for data in data_list:
-            if artist_name in data:
-                for key in data:
-                    if (key == 'album'):
-                        for album in data[key]:
-                            albums_list.append(album['name'])
+    for data in data_list:
+        if artist_name in data:
+            for key in data:
+                if (key == 'album'):
+                    for album in data[key]:
+                        albums_list.append(album['name'])
     print(*albums_list, sep=", ")
     file.close()
 
@@ -43,17 +34,15 @@ def get_albums_by_artist_name(self, artist_name):
 def get_most_popular_songs_by_artist_name(self, artist_name):
     file = open(DataConfigurator.tracks_path + "tracks.json")
     data_list = json.load(file)
-    albums_list = []
-    popular_songs = [0]*10
-    for list in data_list:
-        for data in data_list:
-            if artist_name in data:
-                for i in range(0, 10):
-                    max = (data['song_popularity'])
-                    for j in range(len(popular_songs)):
-                        if popular_songs[j] > max:
-                            max = popular_songs[j]
-                        popular_songs.append(max)
+    popular_songs = [0] * 10
+    for data in data_list:
+        if artist_name in data:
+            for i in range(0, 10):
+                max = (data['song_popularity'])
+                for j in range(len(popular_songs)):
+                    if popular_songs[j] > max:
+                        max = popular_songs[j]
+                    popular_songs.append(max)
     print(*popular_songs, sep=", ")
     file.close()
 
