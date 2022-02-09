e10e7976e49f2496ab204fe69939212f73cb1788
Noa <noa27102@gmail.com>
2022-02-09 13:19:40 +0200

    Decided to save data using files and not MySQL.

--- workspace/Core/Database.py
+++ workspace/Core/Database.py
@@ -1,24 +1,32 @@
-import mysql.connector
+from workspace.ETL.Load import Load
+from workspace.Entities.User import User
 
 
 class Database:
     def __init__(self):
-        self.db_connector = mysql.connector.connect(host="localhost")
-        self.my_cursor = self.db_connector.cursor()
+        self._database_name = "users_data.txt"
+        self._file_path = r"C:\BasicCourse\Tasks\Spotipy\users"
+        self._file_loader = Load()
 
     def create_db(self):
-        self.my_cursor.execute("CREATE DATABASE users_data")
-        self.my_cursor.execute("CREATE TABLE users (username VARCHAR(255) PRIMARY KEY, password VARCHAR(255))")
+        self._file_loader.create_file("hello", self._file_path, self._database_name)
 
     def add_user_to_db(self, username, password):
-        sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-        val = (username, password)
-        self.my_cursor.execute(sql, val)
+        user = User(username, password)
+        self._file_loader.load_data_to_file(user, self._file_path, self._database_name)
 
-    def check_if_user_exist(self, username, password):
-        self.my_cursor.execute(f"SELECT {username} FROM users WHERE username=?")
-        check_username = self.my_cursor.fetchone()
-        if check_username != 0:
-            print(f"Username {username} doesn't exist")
-        else:
-            print(f"{username} logged in!")
+    def check_if_user_exist(self, username):
+        file = self._file_loader.get_data_from_file(self._file_path, self._database_name)
+        for line in file.readlines():
+            if username in line:
+                return True
+        return False
+
+    def check_password_validation(self, username, password):
+        file = self._file_loader.get_data_from_file(self._file_path, self._database_name)
+        lines = file.readlines()
+        for line in lines:
+            if username in line:
+                if password in line:
+                    return True
+        return False
