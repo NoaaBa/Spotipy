502e2017c3779da9eff6e4058c42c305127b0768
Noa <noa27102@gmail.com>
2022-02-09 17:29:49 +0200

    Added data configurator in order to be able to change the paths across the project more easily

---
+++ workspace/Storage/DataConfigurator.py
@@ -0,0 +1,4 @@
+class DataConfigurator:
+    songs_path = r"C:\BasicCourse\Tasks\Spotipy\songs"
+    tracks_path = r"C:\BasicCourse\Tasks\Spotipy\tracks\\"
+    users_path = r"C:\BasicCourse\Tasks\Spotipy\users\\"
