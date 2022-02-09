c177ea88b942cd2c57a869757383308328d08e80
Noa <noa27102@gmail.com>
2022-02-09 18:59:37 +0200

    Added variables that present the max of playlists/songs that a non-prime user can add to a playlist

--- workspace/Entities/User.py
+++ workspace/Entities/User.py
@@ -1,40 +1,44 @@
 import json
 
-
 class User:
     def __init__(self, username, password, playlists=None, premium=None):
-        self._username = username
-        self._password = password
-        self._songs_playlists = playlists
-        self._is_premium = premium
+        self.username = username
+        self.password = password
+        self.songs_playlists = playlists
+        self.is_premium = premium
+
+         # The number of playlists a non-premium user can have.
+        self.max_non_premium_playlists = 5
+         # The number of songs in playlists a non-premium user can have.
+        self.max_non_premium_songs = 20
+
 
     def create_playlist(self):
-        if self._is_premium is False:
+        if self.is_premium is False:
             # TODO LOGS
-            if len(self._songs_playlists) > 5:
+            if len(self.songs_playlists) > self.max_non_premium_playlists:
                 print("You've reached the limit of playlists you can create.\n"
                       "Consider buying Premium in order to be able to create more playlists.")
                 return
         playlist_name = input("Enter playlist name: ")
-        if playlist_name in self._songs_playlists:
+        if playlist_name in self.songs_playlists:
             print("Name already exists!")
         else:
-            self._songs_playlists.append(playlist_name)
+            self.songs_playlists.append(playlist_name)
 
     def add_songs_to_playlist(self):
         playlist_name = input("Enter playlist name: ")
         # TODO LOGS
-        if playlist_name not in self._songs_playlists:
+        if playlist_name not in self.songs_playlists:
             print("Playlist doesn't exist.")
         else:
-            if self._is_premium is False:
-                if len(self._songs_playlists[playlist_name]) > 20:
+            if self.is_premium is False:
+                if len(self.songs_playlists[playlist_name]) > self.max_non_premium_songs:
                     print("You've reached the limit of songs you can put in a playlist.\n"
                           "Consider buying Premium in order to be able to add more songs.")
                 return
             song_name = input("Enter song name: ")
-            self._songs_playlists[playlist_name].append(song_name)
+            self.songs_playlists[playlist_name].append(song_name)
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=False, indent=4)
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
